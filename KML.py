10bf2a6e39eff2e4985619d47c56072d2e82ace1
Josephine <josephine.esposito.2000@gmail.com>
2022-10-01 17:45:05 -0500

    Update KML.py

--- KML.py
+++ KML.py
@@ -1,6 +1,11 @@
 import p # I dati modificabili sono qui!
 
+# KML Parser library, need to install lxml and pykml first!
+import lxml
+from pykml import parser
+from lxml.etree import tostring
 
+from os import path
 
 # Funzione per cercare substring dentro string
 def found(str, search):
@@ -11,4 +16,35 @@ def found(str, search):
     return found
 
 # Variabili
-kml_file = open(p.name_file, "r")
+kml_file = p.name_file
+
+with open(kml_file) as f:
+    doc = parser.parse(f)
+
+inner_html = tostring(doc) # Questo é tutto il kml in string
+# print(inner_html)
+kml_string = inner_html.decode('UTF-8')
+a = type(kml_string)
+print(a)
+
+i = 0
+while i != 0:
+    kml_string.pop(-1)
+    i = kml_string[-1]
+
+s = "c"
+coor_str = "<"
+while s != "t":
+    coor_str.append(kml_string[-1])
+coor_str.append(">")
+
+# Matrice di coordinate: latitudine, longitudine e altitudine
+lat = ["0"]
+lon = ["0"]
+alt = ["0"]
+coordinates = [lat, lon, alt]
+
+stop_chr = "<"
+# while stop_chr != ">":
+
+print(kml_string)
